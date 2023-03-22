b53cd71535a2e5af9fcfef6bef85050a22da7408
Breksam Elsokkary <76849788+Breksam@users.noreply.github.com>
2023-03-22 02:11:31 +0200

    Add files via upload

---
+++ includes/templetes/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-nav" aria-expanded="false">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="app-nav">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="employees.php">Employess</a></li>
+        <li><a href="add.php">Add Employee</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h2 class="p-3 col text-center mt-5 alert alert-info"><?php getHeading();?></h2>
+        </div>
+    </div>
+</div>
